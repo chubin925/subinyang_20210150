4c324d123cb1d7055dd0c69143bab7ab3d750e99
chubin <befanyelse@gmail.com>
2026-04-16 17:39:28 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,45 +1,28 @@
-let inputThing;
-let numberThing;
-let inputted = " ";
-let numberInputted = " ";
-
-let nameButton;
-let numberButton;
-
+let x = 50;
+let y = 200;
+let wallX = 300; // 벽의 X 좌표
 function setup() {
-  createCanvas(400, 400);
-  inputThing = createInput();
-  inputThing.position(60, 35);
-  inputThing.size(200);
-
-  numberThing = createInput();
-  numberThing.position(60, 35);
-  numberThing.size(200);
-
-  nameButton = createInput();
-  nameButton.position(60, 5);
-  numberButton = createInput();
-  numberButton.position(60, 5);
-
-  nameButton.mousePressed(namename);
-  numberButton.mousePressed(numbernumber);
+  createCanvas(600, 400);
 }
-
 function draw() {
-  background(255, 255, 255);
-  text("이름: ", 20, 20);
-  text("학번: ", 20, 50);
-  textSize(16);
-  
-  fill(255, 0, 0);
-  text(inputted, 200, 200);
-  text(numberInputted, 200, 250);
-}
+  background(220);
+  // 1. 벽 그리기
+  stroke(255, 0, 0);
+  strokeWeight(4);
+  line(wallX, 0, wallX, height);
+  // 2. 이동 로직 (벽 체크 포함)
+  if (keyIsDown(RIGHT_ARROW)) {
+    // 다음 이동할 위치(x + 5)가 벽(wallX)보다작을때만이동!
+    if (x + 25 < wallX) {
+      x += 5;
+    }
+  }
 
-function namename() {
-  inputted = inputThing.value();
-}
-
-function numbernumber() {
-  numberInputted = numberInputted.value();
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+  }
+  // 3. 플레이어 그리기
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(x, y, 50, 50);
 }
