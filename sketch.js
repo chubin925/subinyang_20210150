b7d063aa492f1ac48f0700af6e94bf59ceeeb487
chubin <befanyelse@gmail.com>
2026-04-28 16:51:31 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let px, py; 
-let pd = 24; 
+let px, py;
+let pd = 24;
 let speed = 3;
 let dir = "right"; //방향
 
@@ -14,10 +14,20 @@ let roadRects = [];
 let outerLines = [];
 let wallRects = [];
 
-//콩 배열
-let beanX = [];
-let beanY = [];
-let beanActive = [];
-let beanSize = 7;
+//콩, 적 만들어야댐
 
-// 적 배열
+function setup() {
+  createCanvas(1024, 560);
+
+  // 길
+  roadRects = [];
+
+  // 바깥 테두리
+  outerLines = [];
+}
+
+function draw() {
+  background(0);
+}
+
+
